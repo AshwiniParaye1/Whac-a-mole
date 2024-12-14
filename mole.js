536875f74e1feb6c154cc9219ef28e1a1b76be11
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-14 14:24:06 +0530

    setting mole on tiles

--- mole.js
+++ mole.js
@@ -9,4 +9,20 @@ function setGame() {
     tile.id = i.toString();
     document.getElementById("board").appendChild(tile);
   }
+
+  setInterval(setMole, 2000);
+}
+
+function getRandomTile() {
+  let num = Math.floor(Math.random() * 9);
+  return num.toString();
+}
+
+function setMole() {
+  let mole = document.createElement("img");
+  mole.src = "./monty-mole.png";
+
+  let num = getRandomTile();
+  currMoleTile = document.getElementById(num);
+  currMoleTile.appendChild(mole);
 }
