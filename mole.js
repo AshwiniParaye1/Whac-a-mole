457495b640e5390f2152fc1471cbeefb60849e21
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-12-14 14:16:22 +0530

    added pipes for mole

---
+++ mole.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  //setitng up the grid for the game board in html
+  for (let i = 0; i < 9; i++) {
+    let tile = document.createElement("div");
+    tile.id = i.toString();
+    document.getElementById("board").appendChild(tile);
+  }
+}
